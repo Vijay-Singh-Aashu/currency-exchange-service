b3f541f2a7f580ed09e459ccb1f1c4a01af1e3c0
Vijay Singh <vijaysinghaashu@gmail.com>
2023-10-13 12:27:58 +0530

    add-zipkin

--- src/main/java/com/excellence/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/excellence/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -17,16 +17,10 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
-	// @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-	// @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-	// @RateLimiter(name="default")
 	@Bulkhead(name = "sample-api")
 	// 10s => 10000 calls to the sample api
 	public String sampleApi() {
 		logger.info("Sample api call received");
-//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", 
-//					String.class);
-//		return forEntity.getBody();
 		return "sample-api";
 	}
 
